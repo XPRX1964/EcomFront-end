4e2c54564dde9f46130158fb4ff4f2e206ab425c
XPRX1964 <119813877+XPRX1964@users.noreply.github.com>
2024-10-08 18:50:25 +0100

    eye to Eye

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import Eye from "./eye";
+import Eye from "./Eye";
 import EyeSee from "./EyeSee";
 
 function Register() {
